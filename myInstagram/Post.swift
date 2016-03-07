b2c0284ebee105f1db4b687a3f5bd0b47f064e26
Erica Lee <ericanylee@gmail.com>
2016-03-07 10:24:41 -0600

    photo uploading features need to be added

---
+++ myInstagram/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  myInstagram
+//
+//  Created by Erica Lee on 3/6/16.
+//  Copyright © 2016 Erica Lee. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
---
+++ myInstagram/photoCell.swift
@@ -0,0 +1,24 @@
+//
+//  photoCell.swift
+//  myInstagram
+//
+//  Created by Erica Lee on 3/6/16.
+//  Copyright © 2016 Erica Lee. All rights reserved.
+//
+
+import UIKit
+
+class photoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
